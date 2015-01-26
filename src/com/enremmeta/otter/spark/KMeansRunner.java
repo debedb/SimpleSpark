548ebe7d6199021b859614f1b98089f61244f4f2
Gregory Golberg <grisha@alum.mit.edu>
2015-01-25 21:42:44 -0800

    write

--- src/com/enremmeta/otter/spark/KMeansRunner.java
+++ src/com/enremmeta/otter/spark/KMeansRunner.java
@@ -1,5 +1,8 @@
 package com.enremmeta.otter.spark;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
@@ -8,6 +11,12 @@ import org.apache.spark.mllib.clustering.KMeans;
 import org.apache.spark.mllib.clustering.KMeansModel;
 import org.apache.spark.mllib.linalg.Vector;
 import org.apache.spark.mllib.linalg.Vectors;
+import org.apache.spark.rdd.RDD;
+
+import scala.collection.JavaConversions;
+import scala.collection.mutable.Buffer;
+import scala.collection.mutable.Seq;
+import scala.reflect.ClassTag;
 
 public class KMeansRunner extends ServiceRunner implements Constants {
 
@@ -27,17 +36,18 @@ public class KMeansRunner extends ServiceRunner implements Constants {
 
     public KMeansRunner(String[] argv) throws Exception {
 	super();
-	getOpts().addOption("f", true, "HDFS file");
+	getOpts().addOption("i", true, "Input - HDFS file");
+	getOpts().addOption("o", true, "Output - HDFS file");
 	getOpts().addOption("c", true, "Cluster count");
 	getOpts().addOption("m", true, "Max iterations");
 	parseCommandLineArgs(argv);
-	this.file = getCl().getOptionValue('f');
+	this.inFile = getCl().getOptionValue('i');
 	String clusterCountStr = getCl().getOptionValue('c');
 	if (clusterCountStr == null) {
 	    clusterCountStr = "2";
 	}
 	this.clusterCount = Integer.parseInt(clusterCountStr);
-	
+
 	String maxIterationsStr = getCl().getOptionValue('m');
 	if (maxIterationsStr == null) {
 	    maxIterationsStr = "50";
@@ -48,8 +58,8 @@ public class KMeansRunner extends ServiceRunner implements Constants {
 
     private int clusterCount;
     private int maxIterations;
-    private String file;
-
+    private String inFile;
+    private String outFile;
     public static final String HDFS_PREFIX = "hdfs://ip-10-51-152-144.ec2.internal/user/impala/x5_2/";
 
     public void runKmeans() {
@@ -58,7 +68,7 @@ public class KMeansRunner extends ServiceRunner implements Constants {
 	JavaSparkContext sc = new JavaSparkContext(conf);
 
 	// Load and parse data
-	String path = HDFS_PREFIX + file;
+	String path = HDFS_PREFIX + inFile;
 	JavaRDD<String> data = sc.textFile(path);
 	JavaRDD<Vector> parsedData = data.map(new ParsingMapper());
 
@@ -68,6 +78,16 @@ public class KMeansRunner extends ServiceRunner implements Constants {
 					    maxIterations);
 
 	// Evaluate clustering by computing Within Set Sum of Squared Errors
+	Vector[] centers = clusters.clusterCenters();
+	List<Vector> centersList = Arrays.asList(centers);
+	Buffer<Vector> buf = JavaConversions.asScalaBuffer(centersList);
+	Seq<Vector> seq = buf.seq();
+	ClassTag<Vector> tag = scala.reflect.ClassTag$.MODULE$
+		.apply(Vector.class);
+	RDD<Vector> outRdd = sc.sc().makeRDD(seq, clusterCount, tag);
+	String pathOut = HDFS_PREFIX + outFile;
+	outRdd.saveAsTextFile(pathOut);
+
 	double WSSSE = clusters.computeCost(parsedData.rdd());
 	System.out.println("Within Set Sum of Squared Errors = " + WSSSE);
     }
